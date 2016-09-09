378010a3754b6785dc288dfc4e4ca67c8c22231b
w.woloszyn <woloszyn.w.a@gmail.com>
2016-09-09 16:46:18 +0200

    Implement graph transposition

--- SCC/main.cpp
+++ SCC/main.cpp
@@ -3,7 +3,21 @@
 #include<sstream>
 #include<list>
 #include<vector>
+
 using namespace std;
+
+vector< list<int> > Reverse_Graph(vector< list<int> > G)
+{
+	int n = G.size();
+	vector< list<int> > Gt(n);
+	for (int i = 0; i < n; i++)
+		for (list<int>::iterator it = G[i].begin(); it != G[i].end(); it++)
+		{
+			Gt[*it].push_back(i);
+		}
+
+	return Gt;
+}
 
 int main()
 {
@@ -16,6 +30,7 @@ int main()
 	cin.ignore();
 
 	vector< list<int> > graph(n);
+	vector< list<int> > graph_transposed;
 
 	while (getline(cin, line))
 	{
@@ -27,6 +42,8 @@ int main()
 		head--; //beacause C countrs from 0
 		graph[tail].push_back(head);
 	}
+
+	graph_transposed = Reverse_Graph(graph);
 
 	return 0;
 }
