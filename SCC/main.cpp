4676a85d670319ed18dccb72215e8dae6df6ad4c
w.woloszyn <woloszyn.w.a@gmail.com>
2016-09-09 21:02:22 +0200

    Implement first dfs loop (magical ordering)

--- SCC/main.cpp
+++ SCC/main.cpp
@@ -6,9 +6,8 @@
 
 using namespace std;
 
-vector< list<int> > Reverse_Graph(vector< list<int> > G)
+vector< list<int> > ReverseGraph(vector< list<int> >& G, int n)
 {
-	int n = G.size();
 	vector< list<int> > Gt(n);
 	for (int i = 0; i < n; i++)
 		for (list<int>::iterator it = G[i].begin(); it != G[i].end(); it++)
@@ -17,6 +16,45 @@ vector< list<int> > Reverse_Graph(vector< list<int> > G)
 		}
 
 	return Gt;
+}
+
+void FirstDFS(vector< list<int> >& G, vector<int>& F, vector<bool>& E, int start, int& nodes_processed)
+{
+	E[start] = true;
+	for (list<int>::iterator it = G[start].begin(); it != G[start].end(); it++)
+	{
+		if (E[*it] == false)
+		{
+			FirstDFS(G, F, E, *it, nodes_processed);
+		}
+	}
+	
+	nodes_processed++;
+	F[start] = nodes_processed;
+}
+
+void FirstDFSLoop(vector< list<int> >& G, vector<int>& F, vector<bool>& E, int n)
+{
+	int nodes_processed = 0;
+	for (int start = n - 1; start >= 0; start--)
+	{
+		if (E[start] == false)
+		{
+			FirstDFS(G, F, E, start, nodes_processed);
+		}
+	}
+}
+
+void SCC(vector< list<int> >& G)
+{
+	int n = G.size();
+	vector<int> Leaders(n);
+	vector<int> Finish(n);
+	vector<bool> Explored(n, false);
+	vector< list<int> > Grev = ReverseGraph(G, n);
+	
+	FirstDFSLoop(Grev, Finish, Explored, n);
+	int stop = 0;
 }
 
 int main()
@@ -30,7 +68,6 @@ int main()
 	cin.ignore();
 
 	vector< list<int> > graph(n);
-	vector< list<int> > graph_transposed;
 
 	while (getline(cin, line))
 	{
@@ -43,7 +80,7 @@ int main()
 		graph[tail].push_back(head);
 	}
 
-	graph_transposed = Reverse_Graph(graph);
+	SCC(graph);
 
 	return 0;
 }
